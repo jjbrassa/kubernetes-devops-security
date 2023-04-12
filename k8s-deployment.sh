816e308f3378af6436894d0a7e31d826d782b2e5
Jeff Brassard <jbrassard@jbrassard-MBP.local>
2023-04-11 18:49:13 -0600

    fixing vol mnt

--- k8s-deployment.sh
+++ k8s-deployment.sh
@@ -2,7 +2,9 @@
 
 #k8s-deployment.sh
 
-# sed -i "s#replace#${imageName}#g" k8s_deployment_service.yaml
+sed -i "s#replace#${imageName}#g" k8s_deployment_service.yaml
+kubectl -n default apply -f k8s_deployment_service.yaml
+
 # kubectl -n default get deployment ${deploymentName} > /dev/null
 
 # if [[ $? -ne 0 ]]; then
@@ -13,5 +15,3 @@
 #     echo "image name - ${imageName}"
 #     kubectl -n default set image deploy ${deploymentName} ${containerName}=${imageName} --record=true
 # fi
-
-kubectl -n default apply -f k8s_deployment_service.yaml
